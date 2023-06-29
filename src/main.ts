9c1ddc11a451d65149fa12d32170758698bebc06
T1mek <misha1234567890m@gmail.com>
2023-06-29 12:16:13 +0300

    init

--- src/main.ts
+++ src/main.ts
@@ -1,53 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {
-  ExpressAdapter,
-  NestExpressApplication,
-} from '@nestjs/platform-express';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import { ValidationPipe } from '@nestjs/common';
-import * as express from 'express';
-import * as serverless from 'serverless-http';
-import * as helmet from 'helmet';
 
-async function bootstrap(module: any) {
-  const app = express();
-  const nestApp = await NestFactory.create<NestExpressApplication>(
-    module,
-    new ExpressAdapter(app),
-    {
-      cors: true,
-    },
-  );
-
-  // const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-  //   cors: true,
-  // });
-  nestApp.useStaticAssets(join(__dirname, '..', 'assets'));
-
-  nestApp.setGlobalPrefix('/.netlify/functions/server');
-  nestApp.enableCors();
-  nestApp.use(helmet);
-  nestApp.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-    }),
-  );
-  nestApp.use(express.json({ limit: '50mb' }));
-  nestApp.use(express.urlencoded({ limit: '50mb', extended: true }));
-
-  await nestApp.init();
-  return app;
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+  app.useStaticAssets(join(__dirname, '..', 'assets'));
+  await app.listen(3100);
 }
-let cachedHandler: any;
-const proxyApi = async (module: any, event: any, context: any) => {
-  if (!cachedHandler) {
-    const app = await bootstrap(module);
-    cachedHandler = serverless(app);
-  }
-
-  return cachedHandler(event, context);
-};
-
-export const handler = async (event: any, context: any) =>
-  proxyApi(AppModule, event, context);
+bootstrap();
